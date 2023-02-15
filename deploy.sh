c708c082ca17762bc1ee6f5a29ad9decb486f3b3
zwf <871772825@qq.com>
2023-02-15 18:21:10 +0800

    fix：deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-rm -rf /var/lib/jenkins/workspace/space/docs/.vitepress/dist/*
-
-cd /var/lib/jenkins/workspace/space
+cd /var/lib/jenkins/workspace/blog
 
 npm install
 npm run build
 
 # 删除站点文件space
-rm -rf /www/vvv/space/*
+rm -rf /www/vvv/space/dist/*
 
-# 进入jenkins -> workspace -> space
-cd /var/lib/jenkins/workspace/space/docs/.vitepress/dist
+# 进入jenkins -> workspace -> blog
+cd /var/lib/jenkins/workspace/blog/docs/.vitepress/dist
 
 # 迁移过去
-cp -rf * /www/vvv/space/
+cp -rf * /www/vvv/blog/dist/
