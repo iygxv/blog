e61e4a1c1d41da62086f23db8dbe9651de9b8630
zwf <871772825@qq.com>
2023-03-08 15:40:47 +0800

    feat： 3月记录

--- docs/.vitepress/sidebar/daily-study/2023.js
+++ docs/.vitepress/sidebar/daily-study/2023.js
@@ -2,6 +2,7 @@ import { getCurItems } from `../../../utils`
 const commonPath = `/article/daily-study/2023`
 const JanuaryDate = getCurItems(`${commonPath}/January`, true)
 const FebruaryDate = getCurItems(`${commonPath}/February`, true)
+const MarchDate = getCurItems(`${commonPath}/March`, true)
 
 const itemsJanuary = JanuaryDate.map(item => ({
   text: item,
@@ -10,6 +11,10 @@ const itemsJanuary = JanuaryDate.map(item => ({
 const itemsFebruary = FebruaryDate.map(item => ({
   text: item,
   link: `${commonPath}/February/${item}.md`
+}))
+const itemsMarch = MarchDate.map(item => ({
+  text: item,
+  link: `${commonPath}/March/${item}.md`
 }))
 
 export default [
@@ -24,5 +29,11 @@ export default [
     collapsible: true,
     collapsed: false,
     items: itemsFebruary
+  },
+  {
+    text: `三月`,
+    collapsible: true,
+    collapsed: false,
+    items: itemsMarch
   },
 ]
