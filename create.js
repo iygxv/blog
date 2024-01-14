d682a57d6c13e08299c9bdd960d946c4e6202131
sy <871772825@qq.com>
2024-01-14 22:39:11 +0800

    feat：blob

--- create.js
+++ create.js
@@ -3,10 +3,11 @@ const path = require('path');
 
 // const folderPath = __dirname + '/docs/coding'; 
 
-// const folderPath = __dirname + '/docs/git'; 
+const folderPath = __dirname + '/docs/git'; 
 
 // const folderPath = __dirname + '/docs/devOps'; 
-const folderPath = __dirname + '/docs/uniapp'; 
+
+// const folderPath = __dirname + '/docs/uniapp'; 
 
 // const folderPath = __dirname + '/docs/informalEssay'; 
 
--- docs/git/index.md
+++ docs/git/index.md
@@ -4,6 +4,7 @@ hidden: true
 
 ## Git
 * [commit提交规范](./commit提交规范.md)
+* [git报错收集](./git报错收集.md)
 * [mac安装oh-my-zsh](./mac安装oh-my-zsh.md)
 * [sourcetree操作](./sourcetree操作.md)
-* [git报错收集](./git报错收集.md)
+* [git-ssh](./git-ssh.md)
